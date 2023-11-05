a3157b4639a8ef566e89ec222de18c30646c160a
Matthew Bannock <69541270+MattBann@users.noreply.github.com>
2023-11-05 14:10:09 +0000

    Attempts to improve mem efficiency

--- overlay2.py
+++ overlay2.py
@@ -23,6 +23,8 @@ class Overlay2:
         self.running = True
         ctx = moderngl.create_context(460, True, False)
         transformer = ImageTransformer(ctx, self.size)
+        crt_image = None
+        cv2.namedWindow('CRTify', cv2.WINDOW_AUTOSIZE)
         while True:
             image = ImageGrab.grab((self.pos[0],self.pos[1],self.size[0],self.size[1])).convert('RGB')
 
@@ -38,13 +40,14 @@ class Overlay2:
             self.prevTime = now
             if cv2.waitKey(int(max(self.frame_time-delta,1))) & 0xFF == ord('q') or not self.running:
                 break
+            
             # if cv2.waitKey(1) & 0xFF == ord('q'):
             #     break
         cv2.destroyAllWindows()
     
     def audio_thread(self):
         stream = AudioStream(input_device_name="CABLE Output (VB-Audio Virtual", 
-                         output_device_name="Headphones (Redmi Buds 3 Pro)")
+                         output_device_name="Speakers (Realtek(R) Audio)")
         stream.plugins = Pedalboard([
             # Resample(samplerate*0.65),
             Bitcrush(8),
